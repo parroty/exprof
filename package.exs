fd5239836036b50ed4a9741277a5d39a90b482ab
parroty <parroty00@gmail.com>
2013-10-13 01:03:08 +0900

    add package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "exprof", description: "A simple code profiler for Elixir using eprof",
+                 version: "0.0.1", keywords: ["Elixir","eprof","profiler"],
+                 maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
+                 repositories: [[github: "parroty/exprof"]])
