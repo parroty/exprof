1a207a8eb0af49c3f12a3508cbf99750973afed9
parroty <parroty00@gmail.com>
2013-10-14 09:14:33 +0900

    add simple test

--- test/exprof_test.exs
+++ test/exprof_test.exs
@@ -1,7 +1,14 @@
 defmodule ExprofTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
   test "the truth" do
     assert(true)
+  end
+
+  test "sample runner" do
+    assert capture_io(fn ->
+      SampleRunner.run
+    end) =~ %r/FUNCTION\s+CALLS/m
   end
 end
