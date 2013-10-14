bcc4387f5ec9b06dba76a2345f80456ef3f93a31
parroty <parroty00@gmail.com>
2013-10-14 09:10:42 +0900

    make internal methods private

--- lib/exprof/reader.ex
+++ lib/exprof/reader.ex
@@ -10,15 +10,15 @@ defmodule ExProf.Reader do
     File.read!(file_name) |> String.split("\n") |> parse([])
   end
 
-  def parse([], acc), do: Enum.reverse(acc)
-  def parse([head|tail], acc) do
+  defp parse([], acc), do: Enum.reverse(acc)
+  defp parse([head|tail], acc) do
     case parse_record(head) do
       nil    -> parse(tail, acc)
       record -> parse(tail, [record|acc])
     end
   end
 
-  def parse_record(head) do
+  defp parse_record(head) do
     case Regex.run(%r/(.+?\/[0-9])\s+(.+?)\s+(.+?)\s+(.+?)\s+\[\s+(.+?)\]/, head) do
       [_all, function, calls, percent, time, us_per_call] ->
         Prof.new(function: function, calls: binary_to_integer(calls), percent: binary_to_float(percent),
