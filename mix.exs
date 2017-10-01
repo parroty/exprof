d73074c1332b3bfca230f1cccdf70007fe47d736
parroty <parroty00@gmail.com>
2017-10-01 16:53:55 +0900

    Update deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ExProf.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ {:exprintf, "~> 0.1"} ]
+    [ {:exprintf, "~> 0.2"} ]
   end
 
   defp description do
