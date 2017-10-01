808bd07acd4943f485e6569929a9d520b8e446d5
parroty <parroty00@gmail.com>
2017-10-01 17:05:37 +0900

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule ExProf.Mixfile do
   end
 
   defp package do
-    [ contributors: ["parroty"],
+    [ maintainers: ["parroty"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/parroty/exprof"} ]
   end
