8b218e61f3768fe83e649e0f8bfc21482a615975
parroty <parroty00@gmail.com>
2018-01-14 20:32:39 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExProf.Mixfile do
 
   def project do
     [ app: :exprof,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.0",
       deps: deps(),
       description: description(),
